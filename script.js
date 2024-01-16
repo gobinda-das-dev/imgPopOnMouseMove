2b61b719a5d5ead715993c1541d995f1630e1012
Gobinda Das <gobinda993720@gmail.com>
2024-01-16 12:58:45 +0530

    using throttleFunction

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.querySelector("#center")
-    .onmousemove = (dets) => {
-        console.log(dets.clientX, dets.clientY)
+const throttleFunction = (func, delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+
+        if (now - prev > delay) {
+            prev = now;
+
+            return func(...args);
+        }
     }
+}
+
+
+document.querySelector("#center")
+    .addEventListener("mousemove",
+        throttleFunction(() => {
+            console.log("container is hovered")
+        }, 300));
